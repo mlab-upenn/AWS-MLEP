682089c0d58b33dd993ebc531aac70a1e32e3ebc
willybernal <willybernal@gmail.com>
2013-09-24 20:17:33 -0400

    added analyzedResults

--- Example1/SATCWROOM/analyzeResults.m
+++ Example1/SATCWROOM/analyzeResults.m
@@ -6,5 +6,10 @@ endTime = 17;
  
 result = resultAnalyzing(csvData, varNo, startTime, endTime);
 
+% Plot All of them 
+for i = 1:27
+    plotCSV(csvData,19,i);
+    str = input('');
+end
 % DR function area & Kickback 
 % 
