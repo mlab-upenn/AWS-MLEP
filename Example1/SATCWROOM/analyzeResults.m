da0c4645036840c6d69eb5f79578a523f76c9c9f
willybernal <willybernal@gmail.com>
2013-09-27 14:26:41 -0400

    updated

--- Example1/SATCWROOM/analyzeResults.m
+++ Example1/SATCWROOM/analyzeResults.m
@@ -1,9 +1,12 @@
 clear all;
-load csvData_weather.mat
+load csvData_125.mat
 
+%% DURING DR 
+% Start/End Time
 startTime = 13;
 endTime = 17;
-%% DURING DR 
+
+% Get Point
 varNo = 19; % CHILLER
 chillerParam = resultAnalyzing(csvData, varNo, startTime, endTime);
 varNo = 18; % FAN
@@ -13,7 +16,7 @@ pumpParam = resultAnalyzing(csvData, varNo, startTime, endTime);
 varNo = 12; % PPD
 ppdParam = resultAnalyzing(csvData, varNo, startTime, endTime);
 
-%% DISPLAY 
+% Get Points
 chillerArray = cell2mat(chillerParam);
 chillPoint = [chillerArray.P6]/1000;
 fanArray = cell2mat(fanParam);
@@ -26,6 +29,7 @@ ppdPoint = [ppdArray.P6];
 totalPoint = chillPoint + fanPoint + pumpPoint;
 
 figure;
+title('During');
 scatter(ppdPoint,chillPoint,'b*');
 hline([chillPoint(1), fanPoint(1), pumpPoint(1), totalPoint(1)],{'b','r','k', 'm'},{'Chiller','Fan','Pump', 'Total'});
 hold on;
@@ -35,24 +39,104 @@ scatter(ppdPoint,pumpPoint,'k*');
 hold on;
 scatter(ppdPoint,fanPoint,'r*');
 hold on;
-for i = 1:27
-    text(ppdPoint(i), chillPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'b');
-    text(ppdPoint(i), fanPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'r');
-    text(ppdPoint(i), pumpPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'k');
-    text(ppdPoint(i), totalPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'm');
+% for i = 1:125
+%     text(ppdPoint(i), chillPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'b');
+%     text(ppdPoint(i), fanPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'r');
+%     text(ppdPoint(i), pumpPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'k');
+%     text(ppdPoint(i), totalPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'm');
+% end
+
+c = 'b';
+for i = [2 3 4 5]
+    text(ppdPoint(i), chillPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), fanPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), pumpPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), totalPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
 end
+
+c = 'r';
+for i = [6 11 16 21]
+    text(ppdPoint(i), chillPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), fanPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), pumpPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), totalPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+end
+
+c = 'k';
+for i = [26 51 76 101]
+    text(ppdPoint(i), chillPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), fanPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), pumpPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+    text(ppdPoint(i), totalPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', c);
+end
+
+% Convex Hull
+K = convhull(ppdPoint, totalPoint);
+
 hold on;
 grid on;
 xlabel('PPD');
 ylabel('PowerkW');
 
+%% KICKBACK
+
+% Start/End Time
+startTime = 17;
+endTime = 19;
+
+% Get Point
+varNo = 19; % CHILLER
+chillerParam = resultAnalyzing(csvData, varNo, startTime, endTime);
+varNo = 18; % FAN
+fanParam = resultAnalyzing(csvData, varNo, startTime, endTime);
+varNo = 22; % CW PUMP
+pumpParam = resultAnalyzing(csvData, varNo, startTime, endTime);
+varNo = 12; % PPD
+ppdParam = resultAnalyzing(csvData, varNo, startTime, endTime);
+
+% Get Points
+chillerArray = cell2mat(chillerParam);
+chillPoint = [chillerArray.P6]/1000;
+fanArray = cell2mat(fanParam);
+fanPoint = [fanArray.P6]/1000;
+pumpArray = cell2mat(pumpParam);
+pumpPoint = [pumpArray.P6]/1000;
+ppdArray = cell2mat(ppdParam);
+ppdPoint = [ppdArray.P6]; 
+
+totalPoint = chillPoint + fanPoint + pumpPoint;
+
+figure;
+title('Kickback');
+scatter(ppdPoint,chillPoint,'b*');
+hline([chillPoint(1), fanPoint(1), pumpPoint(1), totalPoint(1)],{'b','r','k', 'm'},{'Chiller','Fan','Pump', 'Total'});
+hold on;
+scatter(ppdPoint,totalPoint,'m*');
+hold on;
+scatter(ppdPoint,pumpPoint,'k*');
+hold on;
+scatter(ppdPoint,fanPoint,'r*');
+hold on;
+
+for i = 1:125
+    text(ppdPoint(i), chillPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'b');
+    text(ppdPoint(i), fanPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'r');
+    text(ppdPoint(i), pumpPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'k');
+    text(ppdPoint(i), totalPoint(i), ['  ' num2str(i)], 'FontSize', 12, 'color', 'm');
+end
+
+
+hold on;
+grid on;
+xlabel('PPD');
+ylabel('PowerkW');
 
 %% 
-% Plot All of them 
-for i = [1 2 3 4 7 10 19]
-    plotCSV(csvData,19,i);
-    str = input('');
-
-end
-% DR function area & Kickback 
+% % Plot All of them 
+% for i = [1 2 3 4 7 10 19]
+%     plotCSV(csvData,19,i);
+%     str = input('');
+% 
+% end
+% % DR function area & Kickback 
 % 
