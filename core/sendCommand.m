a9ba14ab74b052b53ab799821b4fd684ffb02998
willybernal <willybernal@gmail.com>
2013-12-04 12:24:16 -0500

    sendCommand.m modified with In/Out Stream

--- core/sendCommand.m
+++ core/sendCommand.m
@@ -1,4 +1,14 @@
-function [amazonEC2Client] = sendCommand(amazonEC2Client, publicDnsName, cmd, keyName)
+function sendCommand(publicDnsName, cmd, keyName, feed)
+%SENDCOMMANDFEED A function for sending commands to the EC2 instance
+%   Inputs:
+%       publicDnsName - EC2 Instance public DNS Name
+%       cmd - String with command
+%       keyName - Key to access your EC2 instance
+%       feed - boolean true for displaying output stream
+%
+% (C) 2013 by Willy Bernal(willyg@seas.upenn.edu)
+
+% Last update: 2013-09-11 by Willy Bernal
 
 % JSch
 jsch = com.jcraft.jsch.JSch();
@@ -17,34 +27,35 @@ channel.setCommand(command);
 channel.setErrStream(java.lang.System.err);
 channel.connect();
 
-input = channel.getInputStream();
-% start reading the input from the executed commands on the shell
-str = ones(1,200);
-while (true)
-    while (input.available() > 0)
-        i = 1;
-        cnt = 0;
-        while (i > 0)
-            i = input.read();
-            cnt = cnt+1;
-            str(cnt) = i;
-            if (i < 0)
-                break;
+% check to display output stream
+if feed
+    input = channel.getInputStream();
+    % start reading the input from the executed commands on the shell
+    str = ones(1,200);
+    while (true)
+        while (input.available() > 0)
+            i = 1;
+            cnt = 0;
+            while (i > 0)
+                i = input.read();
+                cnt = cnt+1;
+                str(cnt) = i;
+                if (i < 0)
+                    break;
+                end
             end
+            disp(char(str(1:cnt-1)));
         end
-        disp(char(str(1:cnt-1)));
+        
+        if (channel.isClosed())
+            disp(java.lang.String(['exit-status: ' num2str(channel.getExitStatus())]));
+            break;
+        end
+        
     end
-    
-    if (channel.isClosed())
-        %java.lang.System.out.println('exit-status: ' + channel.getExitStatus());
-        disp(java.lang.String(['exit-status: ' num2str(channel.getExitStatus())]));
-        break;
-    end
-    
-    pause(1);
 end
 
+% disconnect
 channel.disconnect();
 session.disconnect();
-
 end
